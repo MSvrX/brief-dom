aa39434a49b40a811896556812dff4f2174423d9
MSvrX <savreuxmargaux@gmail.com>
2023-06-13 10:29:00 +0200

    complete exercise with the checklist

---
+++ liste-de-courses/courseJS.js
@@ -0,0 +1,17 @@
+let btnSubmit = document.getElementById("btnSubmit");
+let text = document.getElementById("text");
+let ul = document.getElementById("ul");
+
+console.log(btnSubmit);
+console.log(text);
+
+btnSubmit.addEventListener("click", addItem);
+
+function addItem() {
+  let textValue = text.value;
+  if (textValue !== "") {
+    let li = document.createElement("li");
+    li.textContent = textValue;
+    ul.appendChild(li);
+  }
+}
